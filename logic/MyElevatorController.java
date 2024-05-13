a05f2461aa7844637ac7e593efe7c181cbefd1da
Matej Stastny <matysta@outlook.com>
2024-05-13 08:57:28 -0700

    Starter code

---
+++ logic/MyElevatorController.java
@@ -0,0 +1,60 @@
+package logic;
+
+import game.ElevatorController;
+import game.Game;
+
+public class MyElevatorController implements ElevatorController {
+    // Private member data
+    private Game game;
+
+    // Students should implement this function to return their name
+    public String getStudentName() {
+        return "<YOUR NAME>";   // <-- TODO: Replace with your name
+    }
+    public int getStudentPeriod() {
+        return -1;              // <-- TODO: Replace with your class period
+    }
+
+    // Event: Game has started
+    public void onGameStarted(Game game) {
+        this.game = game;
+    }
+
+    // Event: "outside-the-elevator" request, requesting an elevator.
+    //  The event will be triggered with the request is created/enabled & when it is cleared (reqEnable indicates which).
+    public void onElevatorRequestChanged(int floorIdx, Direction dir, boolean reqEnable) {
+        System.out.println("onElevatorRequestChanged(" + floorIdx + ", " + dir + ", " + reqEnable + ")");
+
+        // TODO
+        if (reqEnable) {
+            gotoFloor(0, floorIdx);
+            System.out.println("  --> gotoFloor(" + floorIdx + ")");
+        }
+    }
+
+    // Event: "inside-the-elevator" request, requesting to go to a floor.
+    //  The event will be triggered with the request is created/enabled & when it is cleared (reqEnable indicates which).
+    public void onFloorRequestChanged(int elevatorIdx, int floorIdx, boolean reqEnable) {
+        System.out.println("onFloorRequesteChanged(" + elevatorIdx + ", " + floorIdx + ", " + reqEnable + ")");
+
+        // TODO
+        gotoFloor(0, floorIdx);
+        System.out.println("  --> gotoFloor(" + floorIdx + ")");
+    }
+
+    // Event: Elevator has arrived at the floor & doors are open.
+    public void onElevatorArrivedAtFloor(int elevatorIdx, int floorIdx, Direction travelDirection) {
+        System.out.println("onElevatorArrivedAtFloor(" + elevatorIdx + ", " + floorIdx + ", " + travelDirection + ")");
+
+        // TODO
+    }
+
+    // Event: Called each frame of the simulation (i.e. called continuously)
+    public void onUpdate(double deltaTime) {
+        if (game == null) {
+            return;
+        }
+
+        // TODO
+    }
+}
